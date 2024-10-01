07f047be5f9611ce28e71c8b65bd4b7098ae251e
Huy Nguyen <huyntp1103@gmail.com>
2024-10-01 20:06:14 +0700

    feat: update wiki season I

---
+++ js/wiki-season-script.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  // Navbar
+  let navHeight = $('#navbar').outerHeight(true);
+  $('.qualifying').css('padding-top', navHeight);
+
+  $('#navbar .navbar-nav .nav-item .nav-link:first').addClass('active');
+  $('#navbar .navbar-nav .nav-item .nav-link').click(function (event) {
+    $('#navbar .navbar-nav .nav-item .nav-link').removeClass('active');
+    $(this).addClass('active');
+
+    var targetId = $(this).attr('href');
+    var targetPosition = $(targetId).offset().top;
+    if (targetId !== '#footer') {
+      targetPosition -= navHeight
+    }
+    $('html, body').animate({ scrollTop: targetPosition }, 1 * 1000);
+  });
+  // End navbar
+});
